2fe9b4c4d6ea27daf5cb14587484da64ae0e9b5c
pelloz <pellozhang@163.com>
2018-02-08 17:06:01 +0800

    Initial commit

---
+++ src/main/java/hello/ApiCtrl.java
@@ -0,0 +1,21 @@
+package hello;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiCtrl {
+
+    @RequestMapping("/create")
+    public Content create(@RequestParam(value="content") String content) {
+        Content c = new Content();
+        c.setContent(content);
+        c.setTimeStamp(LocalDateTime.now().toString());
+
+        return c;
+    }
+
+}
---
+++ src/main/java/hello/Content.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class Content {
+
+    private String content;
+    private String timeStamp;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
